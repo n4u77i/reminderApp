fdec8e926cc64c1a370b84a836b16898b5d796d4
n4u77i <nautaxhahmad125@gmail.com>
2022-12-07 01:04:14 +0500

    Creating reminder record in dynamo db now

--- src/functions/setReminder/index.ts
+++ src/functions/setReminder/index.ts
@@ -1,6 +1,7 @@
 import { formatJSONResponse } from "@libs/apiGateway";
 import { dynamo } from "@libs/dynamo";
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { v4 as uuid } from "uuid";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
     try {
@@ -17,12 +18,39 @@ export const handler = async (event: APIGatewayProxyEvent) => {
             return validationErrors
         }
 
+        const userId = email || phoneNumber
+
         const data = {
+            // <body> is spreaded at top because if for some reason <pk> is passed to the event, then it shouldn't override the <pk> we defined below
+            ...body,
+            id: uuid(),
             
+            /**
+             * TTL is value when the record expires
+             * In JS, time is in milli-seconds so <reminderDate> variable will hold the number of milli-secs but TTL requires time in seconds
+             * If <reminderDate> is not converted to secs, TTL will consider milli-secs as seconds and it will take much much longer to expire
+             */
+            TTL: reminderDate / 1000,
+
+            /**
+             * We need to extra columns in the database which will be used to query data
+             * Partion Key (pk): Used to group things by, in our case it is <userId> on which we will group by
+             * Sort key (sk): To define the order in which the query result should come back in
+             * Both <pk> and <sk> need to be string values
+             */
+            pk: userId,
+            sk: reminderDate.toString(),
         }
 
         // Writing to dynamo table
-        return await dynamo.write(data, tableName)
+        await dynamo.write(data, tableName)
+
+        return formatJSONResponse({
+            data: {
+                id: data.id,
+                message: `Reminder is set for ${new Date(reminderDate).toDateString()}`,
+            }
+        })
 
     } catch (error) {
         console.log('Error', error)
@@ -50,7 +78,7 @@ const validateInput = ({
         return formatJSONResponse({
             statusCode: 400,
             data: {
-                message: 'Email or phone number is missing to create the reminder'
+                message: 'Email or phone number is required to create the reminder'
             }
         })
     }
